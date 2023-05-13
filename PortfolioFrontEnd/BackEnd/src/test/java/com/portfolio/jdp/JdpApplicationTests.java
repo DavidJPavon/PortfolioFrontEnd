2218e4f145269d466c19f4d646b993ffa8674c39
Jorge Pavon <davidjpavon@gmail.com>
2023-05-13 16:28:33 -0300

    commit12
    Backend

---
+++ PortfolioFrontEnd/BackEnd/src/test/java/com/portfolio/jdp/JdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.jdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
